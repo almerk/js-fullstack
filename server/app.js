f39f47311a82728657338cb9ffe464b77fafe39c
almerk <almerk.work@gmail.com>
2021-03-02 08:31:29 -0500

    Moved client and server to their folders. Client works, server doesn't

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const state = require('./state.js');
+const db = require('./db.js');
 const app = express();
 const SERVER_PORT = 1337;
 
-const db = state(path.resolve('./src/server/db/db.json'));
-Object.keys(db).forEach(key => {
-    const values = db[key];
+const state = db(path.resolve(__dirname + '/db/db.json'));
+Object.keys(state).forEach(key => {
+    const values = state[key];
     app.get(`/${key}`, (req, res) => {res.send(values)});
     
     if(values.length > 0 && values[0].id) {
