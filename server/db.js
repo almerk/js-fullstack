13f3afe24e254f7e83e39fbad3c423e7070aafc8
almerk <almerk.work@gmail.com>
2021-03-05 01:35:57 -0500

    Added dotenv module to parse .env files in development mode

--- server/db.js
+++ server/db.js
@@ -1,15 +1,17 @@
+require('dotenv').config({ path: '../.env' })
 const mongoose = require('mongoose');
 
+const {
+    MONGO_INITDB_ROOT_USERNAME,
+    MONGO_INITDB_ROOT_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
 
-    MONGO_INITDB_ROOT_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME || 'calendario-db-user',
-    MONGO_INITDB_ROOT_PASSWORD = process.env.MONGO_INITDB_ROOT_USERNAME || '123!123_',
-    MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || 'localhost',
-    MONGO_PORT = process.env.MONGO_PORT || 27017, 
-    MONGO_DB = process.env.MONGO_DB || 'calendariodb'
 
+const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-    const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-    console.log(url);
 const options = {
     useNewUrlParser: true,
     connectTimeoutMS: 10000,
