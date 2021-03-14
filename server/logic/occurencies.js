429eaa6346bf6b414d76e3d27d70364b0db54520
almerk <almerk.work@gmail.com>
2021-03-14 13:10:34 -0400

    Added reccurence date checking

--- server/logic/occurencies.js
+++ server/logic/occurencies.js
@@ -2,22 +2,23 @@ const dateAndTime = require('date-and-time');
 const { RRule, RRuleSet, rrulestr } = require('rrule')
 const values = require('../db/random-entities').dates
 
+//This code works only for date borders (with no time)
+function computeOccurencies(dateArray, start, finish) {
+    start.setHours(0); start.setMinutes(0); start.setSeconds(0);
+    finish.setHours(23); finish.setMinutes(59); finish.setSeconds(59);
+    return dateArray.map(v => {
+        let dates = getDates(v, start, finish);//TODO: isExcept mechanism
+        return dates.map(d => ({
+            eventId: v.eventId,
+            dateTime: d,
+            status: getStatus(v, d)
+        }))
+    }).reduce((a, b) => a.concat(b)).sort((a,b) => a.dateTime.value - b.dateTime.value)
+}
 
+module.exports = computeOccurencies;
 
-/* 
-    { 
-        eventId:'1', 
-        dateTime: {
-            value: new Date(),
-            hasTime: true,
-            belonging: null
-        }
-        status: null
-    } 
-*/
-
-
-
+console.log(computeOccurencies(values, new Date(2020, 02, 01), new Date(2021, 04, 30)))
 
 function getDates(date, startDay, finishDay) {
     switch (date.type)
@@ -25,7 +26,7 @@ function getDates(date, startDay, finishDay) {
         case 'simpleDate':
         {
             const dateTime = date.dateTime
-            if((startDay <= dateTime && dateTime <= finishDay) || dateAndTime.isSameDay(dateTime, finishDay)) {
+            if((startDay <= dateTime && dateTime <= finishDay)) {
                 return [{
                     value: dateTime,
                     hasTime: date.hasTime,
@@ -36,7 +37,14 @@ function getDates(date, startDay, finishDay) {
         }
         case 'reccurenceDate':
         {
-            
+            return date.rrules.map(r => {
+                const rrule = rrulestr(r);
+                return rrule.between(startDay, finishDay);
+            }).reduce((a, b) => a.concat(b)).map(x => ({
+                value: x,
+                hasTime: date.hasTime,
+                belonging: null
+            }));
         }
 
         
@@ -44,10 +52,7 @@ function getDates(date, startDay, finishDay) {
     return [];
 }
 
+function getStatus(date, dateTime){
+    return null;
+}
 
-
-values.filter(d=>d.type=='simpleDate').forEach(d => {
-    console.log(d);
-    console.log('////////////////');
-    console.log(getDates(d, new Date(1901, 0, 1), new Date(1950, 11 ,31, 23, 59, 59)));
-})
